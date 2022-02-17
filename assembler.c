0069972508d36d288d3ade17da7580c737783259
Ohad Katz <ohad43@gmail.com>
2022-02-17 06:36:39 -0800

    Message

--- assembler.c
+++ assembler.c
@@ -13,15 +13,18 @@ int main(int argc, char *argv[])
           if ((size = runMacro(argv[arg])) == -1)
                continue;
           
+          /*Allocating memory to text array*/
           text = (char **)malloc(size*(sizeof(char *)));
           for (t = 0; t < size; t++)
                *text = (char *)malloc(MAX_LINE*sizeof(char));
           t = 0;
 
+          /*Opens the Pre Assemblered code*/
           code = fopen("output.txt", "r");
           if (code == NULL) {printf("\nError occured trying to open file"); continue;}
           fseek(code, 0, SEEK_SET);
           
+          /*Saves the code to array text*/
           while (fgets(text[t], MAX_LINE, code));
 
           remove("output.txt");
